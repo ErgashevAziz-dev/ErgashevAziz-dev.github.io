137e730590991275f5aae6d5a667d08a5efea9e9
ergashevaziz-dev <ergashevazizdev@gmail.com>
2025-07-07 11:23:40 +0500

    fixed

--- sharks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SharksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sharks'
--- sharks/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Ism')
-    phone = forms.CharField(max_length=20, label='Telefon raqam')
-    message = forms.CharField(widget=forms.Textarea, label='Xabar')
